ebcdfcc163e286d26fa8ea1fe745515d3c6a9325
Max <maxstevenson@msn.com>
2018-10-25 11:43:29 +0100

    added new test for multiples of 5
    increased the test range of both multiples of 3 and multiples of 5 to 1000
    
    Original code passes all new tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,17 +4,17 @@ describe 'fizzbuzz' do
 	it 'returns "fizz" when passed 3' do
 		expect(fizzbuzz(3)).to eq 'fizz'
 	end
-end
-
-describe 'fizzbuzz' do
 	it 'returns "buzz" when passed 5' do
 		expect(fizzbuzz(5)).to eq 'buzz'
 	end
-end
-
-describe 'fizzbuzz' do
 	it 'returns fizz when passed a multiple of 3' do
-		sample = 3 * rand(100)
+		sample = 3 * rand(1000)
 		expect(fizzbuzz(sample)).to eq 'fizz'
 	end
+	it 'returns buzz when passed a multiple of 5' do
+		expected = "buzz"
+		sample = 5 * rand(1000)
+		expect fizzbuzz(sample) === expected
+	end
 end
+
