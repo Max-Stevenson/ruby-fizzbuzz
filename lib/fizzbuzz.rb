ecad742fa5cd218b97a865f5d720cde334a1bd1f
Max <maxstevenson@msn.com>
2018-10-25 11:54:15 +0100

    test for known multiple of 3 and 5 (15) now passes
    changed order of case statement in original code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz (number)
 	case number
-	when -> (number) {number % 3 == 0}
-	  "fizz"
-	when -> (number) {number % 5 == 0}
-		"buzz"
 	when -> (number) {number % 3 == 0 && number % 5 == 0}
 		"fizzbuzz"
+	when -> (number) {number % 3 == 0}
+		"fizz"
+	when -> (number) {number % 5 == 0}
+		"buzz"
 	end
 end
